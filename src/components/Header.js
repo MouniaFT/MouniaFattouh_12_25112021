59a231b0fb4e8ae13337906e930adda70196dec4
mounia fattouh <mounia.fattouh@gmail.com>
2021-11-25 18:21:21 +0100

    Add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,30 @@
+import logo from '../images/logo.svg'
+import { Link } from 'react-router-dom'
 
+function Header() {
+  return (
+    <div className="header">
+      <div className="container container-l">
+        <Link to="/" className="header_logo">
+          <img src={logo} alt="logo" />
+        </Link>
+        <nav className="header_nav">
+          <Link to="/" className="nav-item">
+            Accueil
+          </Link>
+          <Link to="/" className="nav-item">
+            Profil
+          </Link>
+          <Link to="/" className="nav-item">
+            Réglage
+          </Link>
+          <Link to="/" className="nav-item">
+            Communauté
+          </Link>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Header
