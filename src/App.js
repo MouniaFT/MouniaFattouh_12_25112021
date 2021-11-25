d42e16d06b56f4c79cbbcee27bdd1100eb964ce1
mounia fattouh <mounia.fattouh@gmail.com>
2021-11-25 19:45:09 +0100

    Add FooterSidBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
 import { BrowserRouter as Router } from 'react-router-dom'
+import FooterSidBar from './components/FooterSidBar'
 
 function App() {
   return (
     <Router>
       <Header />
+      <FooterSidBar />
     </Router>
   )
 }
---
+++ src/components/FooterSidBar.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import swimmingIcon from '../images/swimmingIcon.svg'
+import yogaIcon from '../images/yogaIcon.svg'
+import bikingIcon from '../images/bikingIcon.svg'
+import bodybuildingIcon from '../images/bodybuildingIcon.svg'
+
+function FooterSidBar() {
+  return (
+    <footer className="footer">
+      <div className="footer_content">
+        <nav className="footer_nav">
+          <Link to="/" className="footer_icon">
+            <img src={yogaIcon} alt="yogaIcon" />
+          </Link>
+          <Link to="/" className="footer_icon">
+            <img src={swimmingIcon} alt="swimmingIcon" />
+          </Link>
+          <Link to="/" className="footer_icon">
+            <img src={bikingIcon} alt="bikingIcon" />
+          </Link>
+          <Link to="/" className="footer_icon">
+            <img src={bodybuildingIcon} alt="bodybuildingIcon" />
+          </Link>
+        </nav>
+        <p className="footer_copiryght">Copiryght, SportSee 2020</p>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterSidBar
