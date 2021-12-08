7802d930c6f56de57f82b1263006cc85a7ff8d15
mounia fattouh <mounia.fattouh@gmail.com>
2021-12-08 17:01:43 +0100

    Added some modifications in the component ChartLineAverage

--- src/components/ChartLineAverage.js
+++ src/components/ChartLineAverage.js
@@ -1,4 +1,11 @@
-import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
 import { userAverage } from '../mocks/data-user.js'
 import CustomTooltipAverage from './CustomToolTipAverage.js'
 
@@ -11,7 +18,7 @@ function ChartLineAverage() {
    * @returns { String }
    */
   function getWeekDay(day) {
-    const week = { 1: 'L', 2: 'M', 3: 'M', 4: 'J', 5: 'V', 6: 'S', 7: 'S' }
+    const week = { 1: 'L', 2: 'M', 3: 'M', 4: 'J', 5: 'V', 6: 'S', 7: 'D' }
     return week[day]
   }
 
@@ -25,16 +32,16 @@ function ChartLineAverage() {
           data={user.sessions}
           margin={{
             top: 5,
-            right: 30,
-            left: 20,
-            bottom: 20,
+            right: 10,
+            left: 10,
+            bottom: 40,
           }}
         >
           <XAxis
             dataKey="day"
             axisLine={false}
             tickLine={false}
-            tickMargin={10}
+            tickMargin={30}
             tickFormatter={getWeekDay}
             tick={{
               fill: 'rgba(255, 255, 255, 0.5)',
@@ -42,24 +49,25 @@ function ChartLineAverage() {
               fontSize: 12,
             }}
           />
-          <Tooltip
-            content={<CustomTooltipAverage />}
-            cursor={{
-              stroke: 'rgba(0, 0, 0, 0.1)',
-              strokeWidth: 79,
-            }}
-          />
+          <YAxis type="number" domain={[0, 'dataMax + 60']} hide={true} />
+          <Tooltip content={<CustomTooltipAverage />} cursor={false} />
+          <defs>
+            <linearGradient id="linear">
+              <stop offset="30%" stopColor="rgba(255,255,255,0.4)" />
+              <stop offset="100%" stopColor="rgb(255,255,255)" />
+            </linearGradient>
+          </defs>
           <Line
-            type="monotone"
+            type="natural"
             dataKey="sessionLength"
             dot={false}
-            stroke="rgba(255, 255, 255, 0.5)"
+            stroke={' url(#linear)'}
             strokeWidth={2}
             activeDot={{
               fill: 'white',
               stroke: 'rgba(255, 255, 255, 0.2)',
               strokeWidth: 8,
-              r: 4,
+              r: 2,
             }}
           />
         </LineChart>
